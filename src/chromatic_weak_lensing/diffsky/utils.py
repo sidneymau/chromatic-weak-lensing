3d0e66ba300188f5b57ffb5c150b02ddfefca8db
Sidney Mau <sidneymau@gmail.com>
2024-08-09 09:45:16 -0700

    update diffsky utils

--- src/chromatic_weak_lensing/diffsky/utils.py
+++ src/chromatic_weak_lensing/diffsky/utils.py
@@ -1,9 +1,15 @@
 import functools
+import logging
 import os
 import urllib.request
+import warnings
 
 from lsstdesc_diffsky import read_diffskypop_params
 from lsstdesc_diffsky.legacy.roman_rubin_2023.dsps.data_loaders.load_ssp_data import load_ssp_templates_singlemet
+
+
+logger = logging.getLogger(__name__)
+
 
 DSPS_TEST_DATA_URL = "https://portal.nersc.gov/project/hacc/aphearin/lsstdesc_diffsky_data/roman_rubin_2023_z_0_1_cutout_9043.testdata.hdf5"
 DSPS_TEST_DATA = "diffsky.testdata.hdf5"
@@ -14,26 +20,35 @@ MOCK_NAME = "roman_rubin_2023"
 
 
 def retrieve_dsps_test_data():
-    print(f"Retrieving {DSPS_TEST_DATA_URL} > {DSPS_TEST_DATA}")
+    logger.info(f"Retrieving {DSPS_TEST_DATA_URL} > {DSPS_TEST_DATA}")
     status = urllib.request.urlretrieve(DSPS_TEST_DATA_URL, DSPS_TEST_DATA)
     return status
 
 
-def retrieve_dsps_ssp_data():
-    print(f"Retrieving {DSPS_SSP_DATA_URL} > {DSPS_SSP_DATA}")
-    status = urllib.request.urlretrieve(DSPS_SSP_DATA_URL, DSPS_SSP_DATA)
+def retrieve_dsps_ssp_data(fname):
+    logger.info(f"Retrieving {DSPS_SSP_DATA_URL} > {fname}")
+    status = urllib.request.urlretrieve(DSPS_SSP_DATA_URL, fname)
     return status
 
 
 @functools.cache
 def load_dsps_ssp_data():
-    if not os.path.exists(DSPS_SSP_DATA):
-        retrieve_dsps_ssp_data()
-    data = load_ssp_templates_singlemet(DSPS_SSP_DATA)
+    dsps_ssp_data = os.environ.get("DSPS_SSP_DATA")
+    if dsps_ssp_data is None:
+        dsps_ssp_data = DSPS_SSP_DATA
+        warnings.warn(f"DSPS_SSP_DATA not set; downloading to {dsps_ssp_data}")
+        retrieve_dsps_ssp_data(dsps_ssp_data)
+    if not os.path.exists(dsps_ssp_data):
+        warnings.warn(f"DSPS_SSP_DATA set but does not exist; downloading to {dsps_ssp_data}")
+        retrieve_dsps_ssp_data(dsps_ssp_data)
+
+    logger.info(f"loading DSPS_SSP_DATA from {dsps_ssp_data}")
+    data = load_ssp_templates_singlemet(dsps_ssp_data)
     return data
 
 
 @functools.cache
 def load_diffskypop_params():
+    logger.info(f"reading diffsky population parameters for {MOCK_NAME}")
     return read_diffskypop_params(MOCK_NAME)
 
