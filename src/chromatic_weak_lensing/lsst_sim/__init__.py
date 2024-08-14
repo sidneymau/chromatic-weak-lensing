381604b4402ce1ad5366a4b3e9d84c367190f661
Sidney Mau <sidneymau@gmail.com>
2024-08-14 15:42:51 -0700

    add lsst_sim

---
+++ src/chromatic_weak_lensing/lsst_sim/__init__.py
@@ -0,0 +1,3 @@
+from .lsst_sim import (
+    LSST_Sim,
+)
---
+++ src/chromatic_weak_lensing/lsst_sim/lsst_sim.py
@@ -0,0 +1,52 @@
+import logging
+import math
+import os
+import time
+
+import astropy.units as u
+import galsim
+import numpy as np
+
+from dl import queryClient as qc
+
+from chromatic_weak_lensing import utils
+from chromatic_weak_lensing import StellarParams
+
+
+logger = logging.getLogger(__name__)
+
+
+def _query_data_lab(ring, N):
+    """Fetch TRILEGAL stars for a given ra/dec region."""
+    query = \
+       """SELECT
+              mass, ra, dec, logage, logg, logte, logl, z, mu0, av, label, c_o,
+              umag, gmag, rmag, imag, zmag, ymag
+          FROM lsst_sim.simdr2
+          WHERE (ring256={}) AND label=1
+          LIMIT {}
+       """.format(ring, N)
+
+    logger.info(f"Submitting query:\n{query}")
+
+    result = qc.query(sql=query, fmt="structarray")
+
+    return result
+
+
+class LSST_Sim:
+    def __init__(self, data):
+        self.data = data
+
+    def get_params(self, i):
+        sparams = StellarParams(
+            mass=self.data["mass"][i],
+            logg=self.data["logg"][i],
+            logT=self.data["logte"][i],
+            logl=self.data["logl"][i],
+            distance_modulus=self.data["mu0"][i],
+            z=self.data["z"][i],
+            phase=self.data["label"][i],
+            composition=self.data["c_o"][i],
+        )
+        return sparams
