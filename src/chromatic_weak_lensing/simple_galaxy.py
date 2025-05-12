27b59997dc4e0b83254089b10cd0ba7fcd20d45d
Sidney Mau <sidneymau@gmail.com>
2025-05-12 14:05:28 -0700

    support extra arg passing

--- src/chromatic_weak_lensing/simple_galaxy.py
+++ src/chromatic_weak_lensing/simple_galaxy.py
@@ -16,21 +16,21 @@ logger = logging.getLogger(__name__)
 
 
 class SimpleGalaxy(Galaxies):
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self.name = "SimpleGalaxy"
         self.spectrum = Blackbody()
         self.stellar_mass = 1
         self.half_light_radius = 0.3
 
-    def get_morphology(self):
+    def get_morphology(self, *args, **kwargs):
         return galsim.Exponential(half_light_radius=self.half_light_radius)
 
-    def get_spectrum(self):
+    def get_spectrum(self, *args, **kwargs):
         _params = MainSequence.get_params(self.stellar_mass)
         _blackbody_params = self.spectrum.get_params(_params)
         return self.spectrum.get_spectrum(*_blackbody_params)
 
-    def get_galaxy(self):
+    def get_galaxy(self, *args, **kwargs):
         spectrum = self.get_spectrum()
         morphology = self.get_morphology()
 
