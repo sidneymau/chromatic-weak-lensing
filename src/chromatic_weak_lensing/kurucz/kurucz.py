d3c80c8b88aab5e15e526014350e9625af409e31
Sidney Mau <sidneymau@gmail.com>
2025-06-17 17:30:49 -0700

    add interface for kurucz

---
+++ src/chromatic_weak_lensing/kurucz/kurucz.py
@@ -0,0 +1,96 @@
+"""
+"""
+
+import functools
+import logging
+import math
+import os
+from pathlib import Path
+import time
+
+import astropy.units as u
+import galsim
+import pystellibs
+
+
+from chromatic_weak_lensing import utils
+from chromatic_weak_lensing import Stars
+
+
+logger = logging.getLogger(__name__)
+
+
+# Phoenix units
+_wave_type = u.angstrom
+_flux_type = u.erg / u.nm / u.second / u.pc**2
+FLUX_FACTOR = (1 * _flux_type).to(galsim.SED._flambda).value
+
+
+def _get_spectrum(
+    speclib,
+    logt,
+    logl,
+    logg,
+    distance_modulus,
+    z,
+):
+    _start_time = time.time()
+
+    wave_type = "angstrom"
+    flux_type = "flambda"
+
+    wl = speclib._wavelength
+    spec = speclib.generate_stellar_spectrum(
+        logt,
+        logg,
+        logl,
+        z,
+    )
+    surface_area =  utils.get_surface_area(distance_modulus)
+    sed_table = galsim.LookupTable(
+        wl,
+        spec * FLUX_FACTOR / surface_area,
+    )
+
+    sed = galsim.SED(sed_table, wave_type, flux_type)
+
+    _end_time = time.time()
+    _elapsed_time = _end_time - _start_time
+    logger.debug(f"made stellar spectrum in {_elapsed_time} seconds")
+
+    return sed
+
+
+class Kurucz(Stars):
+    def __init__(self):
+        self.name = "Kurucz"
+        self.speclib = pystellibs.Kurucz()
+
+    def get_params(
+        self,
+        stellar_params,
+    ):
+        return (
+            stellar_params.logT,
+            stellar_params.logL,
+            stellar_params.logg,
+            stellar_params.distance_modulus,
+            stellar_params.z,
+        )
+
+    def get_spectrum(
+        self,
+        logt,
+        logl,
+        logg,
+        distance_modulus,
+        z,
+    ):
+        return _get_spectrum(
+            self.speclib,
+            logt,
+            logl,
+            logg,
+            distance_modulus,
+            z,
+        )
